363a7ac2d5417ea24b75b7e60ddb63c85aa63d57
Mazen Shawky <mazenshawqy@gmail.com>
2023-01-21 01:29:18 +0200

    Finish splash screen

--- lib/modules/splash/presentation/screens/splash/splash_screen.dart
+++ lib/modules/splash/presentation/screens/splash/splash_screen.dart
@@ -1,16 +1,61 @@
-import 'package:flutter/material.dart';
-import 'package:responsive_movies_app/config/responsive/responsive_layout.dart';
-import 'package:responsive_movies_app/modules/splash/presentation/screens/splash/mobile_splash_screen.dart';
-import 'package:responsive_movies_app/modules/splash/presentation/screens/splash/tablet_splash_screen.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:responsive_movies_app/config/routes/app_routes.dart';
+import 'package:responsive_movies_app/core/utils/app_colors.dart';
+import 'package:responsive_movies_app/core/utils/app_values.dart';
+import 'package:responsive_movies_app/core/utils/media_query_values.dart';
+import 'package:responsive_movies_app/modules/auth/data/datasources/auth_local_data_source.dart';
+import 'package:responsive_movies_app/app/injection_container.dart' as di;
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
+  final AuthLocalDataSource _authLocalDataSource = di.sl<AuthLocalDataSource>();
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  _goNext() async {
+    _authLocalDataSource.getIsUserLoggedIn().then(
+          (isUserLoggedIn) => {
+            if (isUserLoggedIn)
+              {Navigator.pushReplacementNamed(context, Routes.dashboardRoute)}
+            else
+              {Navigator.pushReplacementNamed(context, Routes.loginRoute)}
+          },
+        );
+  }
+
+  _startDelay() {
+    _timer = Timer(const Duration(milliseconds: 2000), () => _goNext());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const ResponsiveLayout(
-      mobileLayout: MobileSplashScreen(),
-      tabletLayout: TabletSplashScreen(),
+    return Scaffold(
+      backgroundColor: AppColors.primaryLightColor,
+      body: Center(
+          child: Icon(
+        Icons.movie,
+        color: AppColors.primaryColor,
+        size: context.width * AppSize.s0_6_5,
+      )),
     );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 }
